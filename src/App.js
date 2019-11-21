000e067fa11aafcaa63a1737cee6b7f4f6cac2b8
Shahriyar <shahryar102@yahoo.com>
2019-11-21 11:51:43 -0500

    audio

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import AudioAnalyser from "./AudioAnalyser";
+import axios from "axios";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      audio: null
+      audio: null,
+      selectedFile: null,
     };
     this.toggleMicrophone = this.toggleMicrophone.bind(this);
   }
@@ -25,10 +27,38 @@ class App extends Component {
 
   toggleMicrophone() {
     if (this.state.audio) {
+      // console.log(this.state.audio)
       this.stopMicrophone();
     } else {
       this.getMicrophone();
     }
+  }
+
+  sendDataHandler = data => {};
+
+  clearApi = () => {
+    console.log('this is the clear function')
+    axios
+      .delete("https://ironrest.herokuapp.com/deleteCollection/shahriyar")
+      .then(data => {});
+  };
+
+  fileSelectHandler = event =>{
+    this.setState(
+      {
+      selectedFile: (event.target.files[0]),
+      })  
+    }    
+  
+
+  fileUploadHandler=()=>{
+    const fd=FormData();
+    fd.append('image',this.state.selectedFile,this.state.selectedFile.name)
+
+    console.log(fd)
+    // axios.post("https://ironrest.herokuapp.com/deleteCollection/shahriyar",fd).then(res=>{
+    //   console.log(res)
+    // })
   }
 
   render() {
@@ -40,7 +70,23 @@ class App extends Component {
               {this.state.audio ? "Stop microphone" : "Get microphone input"}
             </button>
           </div>
-          {this.state.audio ? <AudioAnalyser audio={this.state.audio} /> : ""}
+          {this.state.audio && (
+            <AudioAnalyser
+              audio={this.state.audio}
+              send={this.sendDataHandler}
+            />
+          )}
+          <button onClick={this.clearApi} style={{"padding":"5px"}}>Clear Niko's API</button><br></br><br></br>
+          <div>
+          <input type='file' onChange={this.fileSelectHandler}/> <br></br>
+          <button onClick={this.fileUploadHandler}>Upload</button>
+          </div>
+         
+
+          
+
+
+
         </main>
       </div>
     );
--- src/AudioAnalyser.js
+++ src/AudioAnalyser.js
@@ -1,9 +1,16 @@
 import React, { Component } from "react";
 import AudioVisualiser from "./AudioVisualiser";
+import axios from 'axios';
+
+
 class AudioAnalyser extends Component {
   constructor(props) {
     super(props);
-    this.state = { audioData: new Uint8Array(0) };
+    this.state = { 
+    
+      audioData: new Uint8Array(0),
+      dataStreem: []
+    };
     this.tick = this.tick.bind(this);
   }
   componentDidMount() {
@@ -16,17 +23,29 @@ class AudioAnalyser extends Component {
     this.rafId = requestAnimationFrame(this.tick);
   }
   tick() {
+    let newDataStreem = [...this.state.dataStreem];
+    newDataStreem.push(this.dataArray)
+    this.setState({
+      dataStreem: newDataStreem
+    })
     this.analyser.getByteTimeDomainData(this.dataArray);
     this.setState({ audioData: this.dataArray });
     this.rafId = requestAnimationFrame(this.tick);
+    console.log(this.state.dataStreem)
   }
   componentWillUnmount() {
+    axios.post("https://ironrest.herokuapp.com/shahriyar", {audio: this.state.dataStreem.slice(0,10)})
+      .then(response => {
+        console.log(response.data)
+      })
     cancelAnimationFrame(this.rafId);
     this.analyser.disconnect();
     this.source.disconnect();
   }
+  
   render() {
     return <AudioVisualiser audioData={this.state.audioData} />;
+    
   }
 }
 
