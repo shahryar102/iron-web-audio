70af6bd299a119df05de29b51762bd1380385d61
Shahriyar <shahryar102@yahoo.com>
2019-11-25 16:25:31 -0500

    gallery saving method modified

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { Switch, Link, Route } from "react-router-dom";
 import Camera from "react-html5-camera-photo";
 import "react-html5-camera-photo/build/css/index.css";
 import Recorder from "react-mp3-recorder";
-import './App.css';
+import "./App.css";
 import "./buttons.css";
 
 class App extends Component {
@@ -18,34 +18,37 @@ class App extends Component {
       selectedFile: null,
       camera: false,
       gallery: [],
-      buttonOn: false,
+      buttonOn: false
     };
   }
 
   componentDidMount() {
     axios.get("https://ironrest.herokuapp.com/shahriyar").then(data => {
-      //set to state and loop through to show images
       console.log(data);
+      console.log(data.data);
       // console.log(data.data[0].url);
       this.setState({ gallery: data.data });
     });
   }
 
   shouldIshowGallery = () => {
-    this.setState({ showGallery: !this.state.showGallery, buttonOn:!this.state.buttonOn });
+    this.setState({
+      showGallery: !this.state.showGallery,
+      buttonOn: !this.state.buttonOn
+    });
   };
 
   showGallery = () => {
-    console.log("gallery");
+    console.log(this.state.gallery);
 
     if (this.state.showGallery) {
       console.log(this.state.gallery);
       return this.state.gallery.map((eachItem, i) => {
         console.log(eachItem.url);
         return (
-          <div className= "gallery" key={i} >
-            <h2>Captured from ironrest #{i+1}</h2>
-            <img src={eachItem.url} width="400px" height="400px" />
+          <div className="gallery" key={i}>
+            <h2>Captured from ironrest #{i + 1}</h2>
+            <img src={eachItem.url} width="800px" height="800px" />
             <button onClick={() => this.delete(i)}>Delete</button>
           </div>
         );
@@ -87,14 +90,14 @@ class App extends Component {
     }
   };
 
-  sendDataHandler = data => {};
+  //sendDataHandler = data => {};
 
   postApi = x => {
     console.log("this is posting function");
     axios
       .post("https://ironrest.herokuapp.com/shahriyar", { url: x })
       .then(data => {
-        console.log(data)
+        console.log(data);
       });
   };
   clearApi = () => {
@@ -113,9 +116,11 @@ class App extends Component {
   checkUploadResult = resultEvent => {
     if (resultEvent.event === "success") {
       console.log(resultEvent, resultEvent.info.url);
-
       this.postApi(resultEvent.info.url);
-      //get the link state
+      let newGallery = [...this.state.gallery];
+    newGallery.push({_id:resultEvent.info.public_id,url:resultEvent.info.url})
+    this.setState({ gallery: newGallery });
+      
     }
   };
 
@@ -148,12 +153,14 @@ class App extends Component {
                 : "Turn on Graphic Microphone"}
             </button>
           </div>
-          
+
           <div>
-            <button className={this.state.buttonOn? "buttonOn" : "buttonOff" }onClick={this.shouldIshowGallery}>
-              {this.state.showGallery? "Hide Gallery" : "Show Gallery"}
+            <button
+              className={this.state.buttonOn ? "buttonOn" : "buttonOff"}
+              onClick={this.shouldIshowGallery}
+            >
+              {this.state.showGallery ? "Hide Gallery" : "Show Gallery"}
             </button>
-            
           </div>
           <button className="buttons" onClick={this.clearApi}>
             Clear iron-rest API
@@ -175,11 +182,9 @@ class App extends Component {
               ? "Turn off React Camera"
               : "Turn on React Camera"}
           </button>
-
-          
         </main>
         <div className="content">
-        {this.state.camera ? (
+          {this.state.camera ? (
             <Camera
               onTakePhoto={dataUri => {
                 this.onTakePhoto(dataUri);
@@ -187,12 +192,12 @@ class App extends Component {
             />
           ) : null}
           {this.state.audio && (
-              <AudioAnalyser
-                audio={this.state.audio}
-                send={this.sendDataHandler}
-              />
-            )}
-            {this.showGallery()}
+            <AudioAnalyser
+              audio={this.state.audio}
+              send={this.sendDataHandler}
+            />
+          )}
+          {this.showGallery()}
         </div>
       </div>
     );
